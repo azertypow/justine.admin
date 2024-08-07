be8746846590488d5e60e4b31c19f4f0000f828e
azertypow <nico@villa1203.ch>
2024-08-07 11:41:44 +0200

    fix: cover name and extends

--- site/config/config.php
+++ site/config/config.php
@@ -3,10 +3,28 @@
 /**
  * The config file is optional. It accepts a return array with config options
  * Note: Never include more than one return statement, all options go within this single return array
- * In this example, we set debugging to true, so that errors are displayed onscreen. 
+ * In this example, we set debugging to true, so that errors are displayed onscreen.
  * This setting must be set to false in production.
  * All config options: https://getkirby.com/docs/reference/system/options
  */
 return [
     'debug' => true,
+    'routes' => [
+        [
+            'pattern' => '/',
+            'action' => function () {
+                go('panel');
+            },
+        ],
+        [
+            'pattern' => '/site-info',
+            'action' => function() {
+                header("Access-Control-Allow-Origin: *");
+                return new Page([
+                    'slug'      => 'projects',
+                    'template'  => 'site-info',
+                ]);
+            }
+        ]
+    ]
 ];
