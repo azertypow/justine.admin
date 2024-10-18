1ac67abd670448ea1e8babe0d907c72f969d451a
azertypow <nico@villa1203.ch>
2024-10-18 07:29:04 +0200

    fix: block about content

--- site/templates/get.about.php
+++ site/templates/get.about.php
@@ -1,5 +1,7 @@
 <?php
 
+include_once '_phpTools/jsonEncodeKirbyContent.php';
+
 function getAbout(Kirby\Cms\App $kirby, Kirby\Cms\Site $site): array
 {
   $contactPage = $site->find('a-propos');
@@ -13,7 +15,31 @@ function getAbout(Kirby\Cms\App $kirby, Kirby\Cms\Site $site): array
     'error' => null,
     'data' => [
       'aboutTitle' => $contactPage->aboutTitle()->value(),
-      'textabout' => $contactPage->textabout()->value(),
+      'textabout' => array_values(
+        $contactPage
+          ->textabout()
+          ->toBlocks()
+          ->map(
+            function ($item) {
+
+              if ($item->type() == 'image' | $item->type() == 'imageGallery') {
+
+                $arrayImages = getImageArrayDataInPage($item->content()->image());
+
+                return array_merge(
+                  $item->toArray(),
+                  [
+                    'images' => $arrayImages ? array_values($arrayImages) : [],
+                  ]
+                );
+              }
+
+              return $item->toArray();
+
+            }
+          )
+          ->data()
+      )
     ],
   ];
 }
